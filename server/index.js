3cd866f46ac3c8418ba4e716227359b1f783955e
jamsheer adiyantakath <jamshe.jamsheer@gmail.com>
2025-01-16 17:20:52 +0530

    create test api routes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const connectDb = require('./config/db')
+const userRoutes = require('./routes/userRoutes')
 const app = express()
 const port = 3000
 
+
+
 connectDb();
+app.use('/user', userRoutes)
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { test } = require('../controllers/userControllers');
+const router = express.Router()
+
+router.get('/test', test)
+
+module.exports = router;
